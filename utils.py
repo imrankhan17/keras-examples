c748186a5aec7c94419f1853a01fcb8677cee20a
Khan, Imran, Vodafone Group <imran.khan18@vodafone.com>
2018-02-02 10:08:56 +0000

    Add random_classifier_accuracy function to utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import numpy as np
 
 
@@ -6,3 +7,10 @@ def vectorise_sequences(sequences, dimension=10000):
     for i, sequence in enumerate(sequences):
         results[i, sequence] = 1.0
     return results
+
+
+def random_classifier_accuracy(labels):
+    labels_copy = np.array(labels).copy()
+    np.random.shuffle(labels_copy)
+    hits = np.array(labels) == labels_copy
+    return sum(hits) / len(hits)
